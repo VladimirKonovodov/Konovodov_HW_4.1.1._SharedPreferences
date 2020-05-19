08805e6917f41993f3a4e3e96e151cc5b94928d0
VladimirKonovodov <kon_vlad@mail.ru>
2020-05-19 20:22:07 +0500

    end 3

--- app/src/main/java/com/example/konovodov_hw_412_sharedpreferences/ListViewActivity.java
+++ app/src/main/java/com/example/konovodov_hw_412_sharedpreferences/ListViewActivity.java
@@ -1,11 +1,11 @@
 package com.example.konovodov_hw_412_sharedpreferences;
 
-import android.content.Context;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -28,20 +28,16 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
     public static final String KEY_2 = "two";
 
     private static String NOTE_TEXT = "note_text";
-    //private EditText mInputNote;
-    //--------------------------------------
+
     private SharedPreferences myNoteSharedPref;
-    //--------------------------------------
+
     private SwipeRefreshLayout mSwipeRefreshLayout;
 
-
     public List<Map<String, String>> mapList = new ArrayList<>();
-    //public Map<String, String> map = new HashMap<>();
-    //private ListView simpleListView;
+
     private ListView list;
     private SimpleAdapter simpleAdapter;
 
-    //private SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +48,6 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
         setSupportActionBar(toolbar);
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
         mSwipeRefreshLayout.setOnRefreshListener(this);
-        // делаем повеселее
-
-        //mSwipeRefreshLayout.setColorSchemeResources(R.color.blue, R.color.green, R.color.yellow, R.color.red);
 
         newPrepareContent(getPref());
         initView();
@@ -62,26 +55,12 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
         simpleAdapter = (SimpleAdapter) createSimpleAdapter(mapList);
         list.setAdapter(simpleAdapter);
 
-
     }
 
     @NonNull
     private BaseAdapter createSimpleAdapter(List<Map<String, String>> values) {
         return new SimpleAdapter(this, values, R.layout.activity_main,
                 new String[]{KEY_1, KEY_2}, new int[]{R.id.oneTttv, R.id.twoTttv});
-        //simpleListView.setAdapter(list);
-    }
-
-    @NonNull
-    private void prepareContent() {
-
-        for (int i = 0; i < getString(R.string.large_text).split("\n\n").length; i++) {
-            Map<String, String> map = new HashMap<>();
-            map.put(KEY_2, String.valueOf(getString(R.string.large_text).split("\n\n")[i].length()));
-            map.put(KEY_1, getString(R.string.large_text).split("\n\n")[i]);
-            mapList.add(map);
-        }
-
 
     }
 
@@ -105,7 +84,10 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
     @NonNull
     private void newPrepareContent(String str) {
         mapList.clear();
-        for (int i = 0; i < str.split("\n\n").length; i++) {
+
+        int subStrCount = str.split("\n\n").length;
+
+        for (int i = 0; i < subStrCount; i++) {
             Map<String, String> map = new HashMap<>();
             map.put(KEY_1, str.split("\n\n")[i]);
             map.put(KEY_2, String.valueOf(str.split("\n\n")[i].length()));
@@ -116,22 +98,17 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
 
 
     private String getPref() {
-
-        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "false");
-
-        if (noteTxt.equals("false")) {
+        if (!myNoteSharedPref.contains(NOTE_TEXT)) {
             return setPref();
-
         } else {
-            return noteTxt;
+            return myNoteSharedPref.getString(NOTE_TEXT, "");
         }
-
     }
 
     private String setPref() {
 
         SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
-        String noteSave = getString(R.string.large_text); //-это данные для сохранения
+        String noteSave = getString(R.string.large_text);
         myEditor.putString(NOTE_TEXT, noteSave);
         myEditor.apply();
         Toast.makeText(ListViewActivity.this, "данные сохранены", Toast.LENGTH_LONG).show();
@@ -140,14 +117,10 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
 
     @Override
     public void onRefresh() {
-        // говорим о том, что собираемся начать
-        //Toast.makeText(this, R.string.refresh_started, Toast.LENGTH_SHORT).show();
-        // начинаем показывать прогресс
 
-        //
         mSwipeRefreshLayout.setRefreshing(false);
         mSwipeRefreshLayout.setEnabled(false);
-        // ждем 0.1 секунды и прячем прогресс
+
         mSwipeRefreshLayout.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -157,9 +130,8 @@ public class ListViewActivity extends AppCompatActivity implements SwipeRefreshL
 
                 mSwipeRefreshLayout.setRefreshing(false);
                 mSwipeRefreshLayout.setEnabled(true);
-                // говорим о том, что собираемся закончить
-                Toast.makeText(ListViewActivity.this, R.string.refresh_finished, Toast.LENGTH_SHORT).show();
+
             }
-        }, 100);
+        }, 0);
     }
 }
